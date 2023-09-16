d456140d71d0eb6987c19c5cedc019f72a69117a
john@2simple.com <john@2simple.com>
2023-09-16 10:09:44 +0100

    Make MyScene into a component

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,32 @@ import { Canvas} from '@react-three/fiber'
 
 import {PerspectiveCamera, OrbitControls} from "@react-three/drei"
 
+/**
+ * A scene consisting of a blue floor
+ * @returns 
+ */
+function MyScene(){ 
+    return (
+        <group>
+
+            <mesh
+                position={[0, -0.05, 0]}
+                rotation-x={Math.PI / 2} 
+                receiveShadow
+            >   
+                <planeGeometry args={[80, 80]}/>
+                <meshStandardMaterial color={"#1565C0"} side={DoubleSide}/>
+            </mesh>
+
+        </group>
+    )
+}
+
+
+
 /** 
  * main component for the whole app
- * contains a camera, a light and a blue rectangular 'floor'
+ * contains a camera, some lights and 'MyScene'
  * */
 function App() {
     return (
@@ -17,14 +40,7 @@ function App() {
             />
             <OrbitControls/>
 
-            <mesh
-                position={[0, -0.05, 0]}
-                rotation-x={Math.PI / 2} 
-                receiveShadow
-            >   
-                <planeGeometry args={[80, 80]}/>
-                <meshStandardMaterial color={"#1565C0"} side={DoubleSide}/>
-            </mesh>
+            <MyScene/>
 
             <ambientLight
                 intensity={1}
