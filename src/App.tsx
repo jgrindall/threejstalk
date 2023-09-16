d16761a34b7f1a90f73048245f5d5f57a188c8f0
john@2simple.com <john@2simple.com>
2023-09-16 10:06:45 +0100

    1. Basic setup.  A camera, a light and a blue floor, with orbit controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import { DoubleSide} from "three"
+
 import { Canvas} from '@react-three/fiber'
+
+import {PerspectiveCamera, OrbitControls} from "@react-three/drei"
 
 /** 
  * main component for the whole app
+ * contains a camera, a light and a blue rectangular 'floor'
  * */
 function App() {
-    return(
-        <Canvas>    
+    return (
+        <Canvas>
+            <PerspectiveCamera
+                makeDefault
+                position={[40, 70, 100]}
+            />
+            <OrbitControls/>
+
+            <mesh
+                position={[0, -0.05, 0]}
+                rotation-x={Math.PI / 2} 
+                receiveShadow
+            >   
+                <planeGeometry args={[80, 80]}/>
+                <meshStandardMaterial color={"#1565C0"} side={DoubleSide}/>
+            </mesh>
+
+            <ambientLight
+                intensity={1}
+            />
         </Canvas>
     )
 }
